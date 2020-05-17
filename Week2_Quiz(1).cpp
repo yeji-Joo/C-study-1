e60be25d9f0e1aa892b61567781fc9e4970c102b
ChaeYubin <63189595+ChaeYubin@users.noreply.github.com>
2020-05-17 19:12:47 +0900

    Update Week2_Quiz(1).cpp

--- Week2_Quiz(1).cpp
+++ Week2_Quiz(1).cpp
@@ -3,25 +3,13 @@ using namespace std;
 
 int main()
 {
-	int a[3][4];
 	int i, k;
 
-	int num = 1;
-
-	for (i = 0; i < 3; i++)
+	for (i = 1; i < 6; i++)
 	{
-		for (k = 0; k < 4; k++)
+		for (k = 0; k < i; k++)
 		{
-			a[i][k] = num;
-			num++;
-		}
-	}
-
-	for (i = 0; i < 3; i++)
-	{
-		for (k = 0; k < 4; k++)
-		{
-			cout << a[i][k] << " ";
+			cout << "*";
 		}
 		cout << endl;
 	}
