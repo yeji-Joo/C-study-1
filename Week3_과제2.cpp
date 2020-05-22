1e36a9492720be9e30f16aaa83b70e538f2d4357
ChaeYubin <63189595+ChaeYubin@users.noreply.github.com>
2020-05-22 23:14:50 +0900

    Add files via upload

---
+++ Week3_과제2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *a, int *b) // 반환형은 void형, 함수 이름은 swap, 매개변수는 두 개의 정수
+{
+	int temp; // 숫자 교환을 위해 int형 변수 하나 더 선언
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main()
+{
+	int num1, num2; // 입력받을 두 개의 변수 선언
+	cout << "first number >> ";
+	cin >> num1;
+	cout << "second number >> ";
+	cin >> num2;
+
+	cout << endl;
+	cout << "swap 전 >> " << num1 << " " << num2 << endl;
+	swap(&num1, &num2);
+	cout << "swap 후 >> " << num1 << " " << num2 << endl;
+}
